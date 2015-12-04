a2f54fa5db9df8ecad219c358b775576c184ccf8
Christopher Williams <abbrev+github@gmail.com>
2015-12-04 15:31:30 -0700

    Make variables const-correct
    PROGMEM variables must be const.

--- Talkie/talkie.h
+++ Talkie/talkie.h
@@ -11,7 +11,7 @@ class Talkie {
   public:
 	Talkie(void);                                 // PWM
 	Talkie(uint8_t cs, uint8_t clk, uint8_t dat); // DAC
-	void    say(uint8_t *address, boolean block=true);
+	void    say(const uint8_t *address, boolean block=true);
 	boolean talking(void); // Poll this when block=false
 };
 
