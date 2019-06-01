60fca6e85f9cd2a837678ff3e90e25f1986f18d2
Phillip Burgess <paintyourdragon@dslextreme.com>
2019-05-31 22:42:53 -0700

    Add TMS5100 mode

--- talkie.h
+++ talkie.h
@@ -7,12 +7,18 @@
 
 #include <Arduino.h>
 
+enum TalkieDevice { // Can emulate two related but not-data-compatible devices
+  TALKIE_TMS5220,   // "Classic" Talkie lib emulates TI 5220 (99/4A, etc.)
+  TALKIE_TMS5100    // Or switch to TI 5100 mode (Speak & Spell, etc.)
+};
+
 class Talkie {
   public:
 	Talkie(void);                                 // PWM
 	Talkie(uint8_t cs, uint8_t clk, uint8_t dat); // DAC
-	void    say(const uint8_t *address, boolean block=true);
-	boolean talking(void); // Poll this when block=false
+	void say(const uint8_t *address, bool block=true);
+	bool talking(void) const; // Poll this when block=false
+	void mode(TalkieDevice mode = TALKIE_TMS5220);
 };
 
-#endif
+#endif // end if _Talkie_h_
